597f2f697bc3b367afc5367374fa35585d8258bf
ctudent <lyudmilka.volkova.70@gmail.com>
2023-05-29 22:04:19 +0800

    initial

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -7,24 +7,24 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV3 {
-    @Test
-    void shouldReturnDemoAccounts() {
+//class MobileBankApiTestV3 {
+   // @Test
+   // void shouldReturnDemoAccounts() {
       // Given - When - Then
       // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
+     // given()
+         // .baseUri("http://localhost:9999/api/v1")
       // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
+     // .when()
+       //   .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200)
+    //  .then()
+         // .statusCode(200)
           // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
+         // .contentType(ContentType.JSON)
+         // .body("", hasSize(3))
+          //.body("[0].currency", equalTo("RUB"))
+        //  .body("[0].balance", greaterThanOrEqualTo(0))
       ;
-    }
-}
+  //  }
+//}
