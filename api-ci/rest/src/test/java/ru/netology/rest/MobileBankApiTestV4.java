7597ec1bba8b38818b58a87a822d2d0af1e1ce97
ctudent <lyudmilka.volkova.70@gmail.com>
2023-06-03 16:34:59 +0800

    commit

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -8,17 +8,20 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                // код теста
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+
     }
 }
